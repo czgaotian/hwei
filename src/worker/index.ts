09345ceebcfbf38f45104fd011242d1bea014aef
czgaotian <811460602@qq.com>
2025-12-25 11:18:33 +0100

    refactor: enhance type safety for Hono app initialization

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -2,12 +2,9 @@ import { Hono } from "hono";
 import { prettyJSON } from "hono/pretty-json";
 import api from "./api";
 
-const app = new Hono();
+const app = new Hono<{ Bindings: Env }>();
 
-const middleware = new Hono<{ Bindings: Env }>();
-
-middleware.use("*", prettyJSON());
-app.route("/api", middleware);
+app.use("/api/*", prettyJSON());
 app.route("/api", api);
 
 export default app;
